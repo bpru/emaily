924ac06dcf2a55e94e2e0993aa4236b5a7f7e16c
Jen-Chieh Chiang <jchiang36@gatech.edu>
2018-12-07 23:25:52 -0500

    fix bug

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import { BrowserRouter, Route } from "react-router-dom";
 import { connect } from "react-redux";
 
-import * as actions from "./actions/";
+import * as actions from "./actions";
 
 import Header from "./components/Header";
 import Landing from "./components/Landing";
--- client/src/index.js
+++ client/src/index.js
@@ -5,7 +5,7 @@ import { createStore, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
 import "materialize-css/dist/css/materialize.min.css";
 
-import App from "./app";
+import App from "./App";
 import reducers from "./reducers";
 
 const store = createStore(reducers, {}, applyMiddleware(thunk));
