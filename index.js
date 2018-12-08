6667aa2c6727a287c411a9ddfb134943e2f50197
Jen-Chieh Chiang <jchiang36@gatech.edu>
2018-12-07 23:31:06 -0500

    fixed typo in index.js

--- index.js
+++ index.js
@@ -49,7 +49,7 @@ if (process.env.NODE_ENV === "production") {
   // Express will serve index.html if it doesnt recognize the route
   const path = require("path");
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirnema, "client", "build", "index.html"));
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
 }
 
